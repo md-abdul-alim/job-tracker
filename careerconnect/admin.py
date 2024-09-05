70cd54f9909c1b95b1370224910b365aac40f07b
Md. Abdul Alim <alim.abdul.5915@gmail.com>
2024-09-05 23:51:28 +0600

    initial commit

---
+++ careerconnect/admin.py
@@ -0,0 +1,68 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import *
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ['name__icontains', 'industry__name__icontains', 'country__name__icontains']
+    list_display = ['name', 'industry', 'country', 'website', 'linkedin_profile', 'remote', 'non_tech']
+    list_filter = ['industry__name', 'country__name', 'remote', 'non_tech']
+    list_display_links = ['name']
+
+
+@admin.register(JobPortal)
+class JobPortalAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'website']
+    list_display = ['name', 'website']
+
+
+@admin.register(JobCategory)
+class JobCategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(WorkPlace)
+class WorkPlaceAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Industry)
+class IndustryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(ConnectionCategory)
+class ConnectionCategoryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    search_fields = ['first_name', 'last_name', 'job_title', 'company__name__icontains',
+                     'job_category__name__icontains', 'connection_category__name__icontains', 'department__name__icontains']
+    list_display = ['first_name', 'company__name', 'connection_category', 'country', 'work_place', 'job_location']
+    list_filter = ['company', 'department', 'work_place', 'job_category', 'connection_category']
+
+
+@admin.register(SearchQuery)
+class SearchQueryAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(VisaOpportunity)
+class VisaOpportunityAdmin(admin.ModelAdmin):
+    search_fields = ['country__name__icontains', 'website__icontains',]
+    list_display = ['country', 'website', ]
+    list_filter = ['country', ]
+
---
+++ careerconnect/models.py
@@ -0,0 +1,103 @@
+from django.db import models
+
+
+class SearchQuery(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class JobPortal(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    website = models.URLField(null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Industry(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    industry = models.ForeignKey(Industry, related_name='companies', on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, related_name='companies', on_delete=models.CASCADE)
+    website = models.URLField(null=True, blank=True, unique=True)
+    linkedin_profile = models.URLField(null=True, blank=True, unique=True)
+    remote = models.BooleanField(default=False)
+    non_tech = models.BooleanField(default=False)
+    phone = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class JobCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ConnectionCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WorkPlace(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    job_title = models.CharField(max_length=255)
+    company = models.ForeignKey(Company, related_name='employees', on_delete=models.CASCADE)
+    job_category = models.ForeignKey(JobCategory, related_name='employees', on_delete=models.CASCADE, null=True, blank=True)
+    connection_category = models.ForeignKey(ConnectionCategory, related_name='employees', on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, related_name='employees', on_delete=models.CASCADE, null=True)
+    country = models.ForeignKey(Country, related_name='employees_country', on_delete=models.CASCADE)
+    job_location = models.ForeignKey(Country, related_name='employees_location', on_delete=models.CASCADE, null=True)
+    work_place = models.ForeignKey(WorkPlace, related_name='employees', on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    linkedin_profile = models.URLField(null=True, blank=True, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class VisaOpportunity(models.Model):
+    country = models.ForeignKey(Country, related_name='visa_country', on_delete=models.SET_NULL, null=True)
+    website = models.URLField(max_length=255, unique=True)
+    link_1 = models.URLField(max_length=255, unique=True, null=True, blank=True)
+    link_2 = models.URLField(max_length=255, unique=True, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.website
